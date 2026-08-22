cff7645ecb1f0303438b99fdd6b8a3943ccd14f4
ItzAurorae <itzaurorae@gmail.com>
2026-08-22 15:57:50 -0400

    Web Analytrics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { DataProvider } from '@/contexts/DataContext';
 import { AuthScreen } from '@/components/auth/AuthScreen';
 import { AppShell } from '@/components/layout/AppShell';
 import { isSupabaseConfigured } from '@/lib/supabase';
+import { Analytics } from "@vercel/analytics/next"
 
 function SupabaseMissingScreen() {
   return (
